7123ef177510314d7420778cc605f5e1fb8b1946
Divino Luiz <92835863+DivinoLuizdev@users.noreply.github.com>
2025-03-26 13:01:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('bom dia')
+import subprocess
+
+# Cria um arquivo de texto com "Bom dia"
+with open("mensagem.txt", "w") as file:
+    file.write("Bom dia")
+
+# Abre o arquivo de texto no Bloco de Notas
+subprocess.Popen(['notepad.exe', 'mensagem.txt'])
